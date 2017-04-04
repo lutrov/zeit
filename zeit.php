50dbe471e0c54a49cd22b1ea10a5f31d021a6dc0
Ivan Lutrov <lutrov@users.noreply.github.com>
2017-04-04 16:20:11 +1000

    Now with hook support.

--- zeit.php
+++ zeit.php
@@ -2,9 +2,11 @@
 
 /*
 Plugin Name: Zeit
-Description: Shows the local server date, time & timezone in the dashboard adminbar. Why this plugin name? Zeit means "time" in German.
+Description: Shows the local server date and time in the dashboard adminbar. Why this plugin name? Zeit means "time" in German.
 Author: Ivan Lutrov
-Version: 1.1
+Author URI: http://lutrov.com/
+Version: 2.0
+Notes: This plugin provides an API to customise the default constant values. See the "readme.md" file for more.
 */
 
 defined('ABSPATH') || die('Ahem.');
@@ -13,16 +15,23 @@ defined('ABSPATH') || die('Ahem.');
 // Constants used by this plugin.
 //
 define('ZEIT_DATE_FORMAT_SHORT', _x('d-M-y H:i', 'timezone date format'));
-define('ZEIT_DATE_FORMAT_LONG', _x('l, j F Y H:i T', 'timezone date format'));
+define('ZEIT_DATE_FORMAT_LONG', _x('l, j F Y H:i', 'timezone date format'));
 
 //
 // Show the time.
 //
+add_action('wp_before_admin_bar_render', 'zeit_server_date_time');
 function zeit_server_date_time() {
 	global $wp_admin_bar;
 	$timezone_format = _x('Y-m-d G:i:s', 'timezone date format');
-	$wp_admin_bar->add_menu(array('id' => 'zeit', 'title' => date_i18n(ZEIT_DATE_FORMAT_SHORT), 'href' => false, 'meta' => array('title' => __('Local server time is ') . date_i18n(ZEIT_DATE_FORMAT_LONG))));
+	$wp_admin_bar->add_menu(array(
+		'id' => 'zeit',
+		'title' => sprintf('<span class="ab-label">%s</span>', date_i18n(apply_filters('zeit_date_format_short_filter', ZEIT_DATE_FORMAT_SHORT))),
+		'meta' => array(
+			'title' => __('Local server time is ') . date_i18n(apply_filters('zeit_date_format_long_filter', ZEIT_DATE_FORMAT_LONG))
+		),
+		'href' => '#'
+	));
 }
-add_action('wp_before_admin_bar_render', 'zeit_server_date_time');
 
 ?>

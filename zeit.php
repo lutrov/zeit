cf1c99b07afa77b177fdc0a9f9897bf885d8d352
Ivan Lutrov <lutrov@users.noreply.github.com>
2017-07-01 16:44:15 +1000

    Update zeit.php

--- zeit.php
+++ zeit.php
@@ -1,11 +1,11 @@
- <?php
+<?php
 
 /*
 Plugin Name: Zeit
 Description: Shows the local server date and time in the dashboard adminbar. Why this plugin name? Zeit means "time" in German.
 Author: Ivan Lutrov
 Author URI: http://lutrov.com/
-Version: 2.0
+Version: 2.1
 Notes: This plugin provides an API to customise the default constant values. See the "readme.md" file for more.
 */
 
@@ -23,18 +23,20 @@ define('ZEIT_DATE_FORMAT_LONG', 'l, j F Y H:i T');
 add_action('wp_before_admin_bar_render', 'zeit_server_date_time');
 function zeit_server_date_time() {
 	global $wp_admin_bar;
-	$long = ZEIT_DATE_FORMAT_LONG;
-	if (substr(date($long), -3) <> 'UTC') {
-		$long = sprintf('%s (\U\T\CP)', $long);
+	if (is_admin() == true) {
+		$long = ZEIT_DATE_FORMAT_LONG;
+		if (substr(date($long), -3) <> 'UTC') {
+			$long = sprintf('%s (\U\T\CP)', $long);
+		}
+		$wp_admin_bar->add_menu(array(
+			'id' => 'zeit',
+			'title' => sprintf('<span class="ab-label">%s</span>', date(apply_filters( 'zeit_date_format_short_filter', ZEIT_DATE_FORMAT_SHORT))),
+			'meta' => array(
+				'title' => __('Local server time is ') . date(apply_filters('zeit_date_format_long_filter', $long))
+			),
+			'href' => '#'
+		));
 	}
-	$wp_admin_bar->add_menu(array(
-		'id' => 'zeit',
-		'title' => sprintf('<span class="ab-label">%s</span>', date(apply_filters( 'zeit_date_format_short_filter', ZEIT_DATE_FORMAT_SHORT))),
-		'meta' => array(
-			'title' => __('Local server time is ') . date(apply_filters('zeit_date_format_long_filter', $long))
-		),
-		'href' => '#'
-	));
 }
 
 ?>

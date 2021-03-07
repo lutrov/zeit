96293372efca5ce39a6d9c0018dfbc6264945542
Ivan Lutrov <lutrov@users.noreply.github.com>
2021-03-07 14:43:50 +1100

    Update zeit.php

--- zeit.php
+++ zeit.php
@@ -2,12 +2,26 @@
 
 /*
 Plugin Name: Zeit
+Version: 2.4
 Description: Shows the local server date and time in the dashboard adminbar. Why this plugin name? Zeit means "time" in German.
 Plugin URI: https://github.com/lutrov/zeit
+Copyright: 2020, Ivan Lutrov
 Author: Ivan Lutrov
 Author URI: http://lutrov.com/
-Version: 2.4
-Notes: This plugin provides an API to customise the default constant values. See the "readme.md" file for more.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+Street, Fifth Floor, Boston, MA 02110-1301, USA. Also add information on how to
+contact you by electronic and paper mail.
 */
 
 defined('ABSPATH') || die('Ahem.');
